7e64038cbb9103b1eaa76f36309bd6444d23da73
Sreenuj121 <145279474+Sreenuj121@users.noreply.github.com>
2023-09-17 20:38:11 +0100

    Delete infra directory

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "DefaultResourceGroup-WUS"
-    storage_account_name = "pythonsan1"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
